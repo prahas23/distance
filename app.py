7560c918ea542f3f4b6df5c5533f78599710072f
Sai Prahas <saiprahas@outlook.com>
2023-12-02 00:54:27 +0530

    fixing

--- app.py
+++ app.py
@@ -2,13 +2,21 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
+metrics_data = 0
+
 @app.route('/')
 def index():
     return render_template('template.html')
 
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    global metrics_data
+    return jsonify(metrics_data)
+
 @app.route('/metrics', methods=['POST'])
 def metrics():
     try:
+        global metrics_data
         data = request.get_json()
         # Assuming the JSON request has a key named 'metrics_data'
         metrics_data = data.get('distance_cm', {})
